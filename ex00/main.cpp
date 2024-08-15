2a48680026f3d292b7afd1260daa6dcfb8d911a8
zom <contact.zomzd@gmail.com>
2024-08-15 12:45:25 +0200

    More precise tests

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,19 +6,40 @@ int main()
 	Bureaucrat bad("BadOne", 2);
 	try
 	{
-		std::cout << good << std::endl;
-		std::cout << bad << std::endl;
-		bad.incrementGrade();
-		std::cout << bad << std::endl;
-		good.decrementGrade();
-		std::cout << good << std::endl;
+		Bureaucrat too_high("Too High", 0);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		Bureaucrat too_low("Too Low", 151);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << std::endl;
+	std::cout << good << std::endl;
+	std::cout << bad << std::endl;
+	bad.incrementGrade();
+	std::cout << bad << std::endl;
+	good.decrementGrade();
+	std::cout << good << std::endl;
+	std::cout << std::endl;
+
+	try
+	{
 		bad.incrementGrade();
 	}
 	catch(const std::exception& e)
 	{
 		std::cout << e.what() << '\n';
 	}
-	
+
 	try
 	{
 		good.decrementGrade();
@@ -27,14 +48,6 @@ int main()
 	{
 		std::cerr << e.what() << '\n';
 	}
-	try
-	{
-		Bureaucrat badInit("BadInit", 124421);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	
+
 	return 0;
 }
